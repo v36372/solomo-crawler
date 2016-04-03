261572434b7787819187d3e6c97aef4926dfe528
Nguyen Trong Tin <nguyentrongtin2331@gmail.com>
2016-04-03 13:42:59 +0700

    * update push function

--- push.py
+++ push.py
@@ -4,6 +4,10 @@ import csv
 
 data = {}
 data["user_token"] = "YPv-QkHTVHq_rxP3yZ7i"
+data["post_type"] = "crawl"
+data["crawl_user_name"] = "Hotdeal.vn"
+data["crawl_user_email"] = "crawler@hotdeal.vn"
+data["crawl_user_avatar"] = "http://www.hotdeal.vn/favicon.ico"
 with open("listdeal.csv") as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
@@ -12,13 +16,9 @@ with open("listdeal.csv") as csvfile:
         data["description"] = row["description"]
         data["location_lat"] = row["lat"]
         data["location_long"] = row["long"]
-        data["post_type"] = "crawl"
-        data["crawl_user_name"] = "Hotdeal.vn"
-        data["crawl_user_email"] = "crawler@hotdeal.vn"
-        data["crawl_user_avatar"] = "http://www.hotdeal.vn/favicon.ico"
         # print json.dumps(data)
         r = requests.post("http://solomo-api.herokuapp.com/api/v1/posts.json",data)
-        break
+        # break
 
 # http://www.hotdeal.vn/favicon.ico
 # r = requests.get("http://solomo-api.herokuapp.com/api/v1/posts.json",data)
